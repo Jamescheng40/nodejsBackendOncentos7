10af51b23bf8c5d6284baa6672c586a1caa8b80a
jamescheng40 <cheng-james@hotmail.com>
2020-04-20 21:48:56 -0400

    testing post

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-var http = require('http');
+//var http = require('http');
+
+
 const express = require('express');
-const router = express.Router();
+const app = express();
+//const router = express.Router();
 const request = require('request-promise-native'); // use Request library + promises to reduce lines of code
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8081, '192.168.1.253');
-console.log('Server running at http://192.168.0.9:8081/');
+// http.createServer(function (req, res) {
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.end('Hello World\n');
+// }).listen(8081, '192.168.1.253');
+// console.log('Server running at http://192.168.0.9:8081/');
 
+app.use(express.urlencoded());
+app.use(express.json());
 
-
-router.post("/" , async (req, res) => {
+app.post("/" , (req, res) => {
 	
-	  const date = req.body.hello;
-  const id = req.body.hello1;
+	 var date = req.body.hello;
+  var id = req.body.hello1;
   
   console.log(date);
   console.log(id);
+res.send(date);
+});
+
+
+app.listen(8081, '192.168.0.9', function()
+{
+	//console.log("... port %d in %s mode", app.address().port, app.settings.env);
+	
 });
