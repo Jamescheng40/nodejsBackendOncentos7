de3412ba3c9e1c133accd31316d2f142ffeb7732
jamescheng40 <cheng-james@hotmail.com>
2020-04-20 21:05:30 -0400

    first commit and pushing backend files

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+const express = require('express');
+const router = express.Router();
+const request = require('request-promise-native'); // use Request library + promises to reduce lines of code
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8081, '192.168.1.134');
+console.log('Server running at http://192.168.0.9:8081/');
+
+
+
+router.post("/" , async (req, res) => {
+	
+	  const date = req.body.hello;
+  const id = req.body.hello1;
+  
+  console.log(date);
+  consol.log(id);
+});
